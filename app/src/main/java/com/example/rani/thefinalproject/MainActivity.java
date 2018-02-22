132bc87623ea2f4120925914f2784c1c15fa00bf
ranyhalloun <31957208+ranyhalloun@users.noreply.github.com>
2018-02-22 15:59:39 +0200

    first version

--- app/src/main/java/com/example/rani/thefinalproject/MainActivity.java
+++ app/src/main/java/com/example/rani/thefinalproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.rani.thefinalproject;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moveToLoginActivity();
     }
+
+    public void moveToLoginActivity(){
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent startActivity = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(startActivity);
+                finish();
+            }
+
+        }, 1500L);
+    }
+
 }
