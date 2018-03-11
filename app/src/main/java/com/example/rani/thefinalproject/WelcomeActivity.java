4f7be249e1445ac4dc3cc4d6fa95d5c85f27c184
nayahashem <34309834+nayahashem@users.noreply.github.com>
2018-03-11 12:39:49 +0200

    12:39

--- app/src/main/java/com/example/rani/thefinalproject/WelcomeActivity.java
+++ app/src/main/java/com/example/rani/thefinalproject/WelcomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -13,6 +14,17 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         moveToLoginActivity();
     }
+
+    protected void onResume() {
+        super.onResume();
+        String tutorialKey = "SOME_KEY";
+        Boolean firstTime = getPreferences(MODE_PRIVATE).getBoolean(tutorialKey, true);
+        if (firstTime) {
+            Toast.makeText(WelcomeActivity.this, "First Run", Toast.LENGTH_LONG).show(); // here you do what you want to do - an activity tutorial in my case
+            getPreferences(MODE_PRIVATE).edit().putBoolean(tutorialKey, false).apply();
+        }
+    }
+
     public void moveToLoginActivity(){
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
