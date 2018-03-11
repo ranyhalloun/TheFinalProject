1debea02af42277e1096e0cad01b9f4f868e81c2
ranyhalloun <31957208+ranyhalloun@users.noreply.github.com>
2018-03-11 12:30:41 +0200

    //sign in

--- app/src/main/java/com/example/rani/thefinalproject/WelcomeActivity.java
+++ app/src/main/java/com/example/rani/thefinalproject/WelcomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.rani.thefinalproject;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,19 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        moveToLoginActivity();
+    }
+    public void moveToLoginActivity(){
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent startActivity = new Intent(WelcomeActivity.this, LogIn_Activity.class);
+                startActivity(startActivity);
+                finish();
+            }
+
+        }, 5000L);
     }
 }
