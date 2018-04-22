35f0be696eb6ab6d98c5651128ef3549ee04bae3
nayahashem <34309834+nayahashem@users.noreply.github.com>
2018-04-22 16:08:03 +0300

    16:00

--- app/src/main/java/com/example/rani/thefinalproject/MainActivity.java
+++ app/src/main/java/com/example/rani/thefinalproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,59 +22,24 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int RC_SIGN_IN = 1;
 
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mMessagesDatabaseReference;
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("main", "MainActivity: in onCreate \n");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    //user is signed in
-                    Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_SHORT).show();
-                } else {
-                    //user is signed out
-                    startActivityForResult(
-                            AuthUI.getInstance()
-                                    .createSignInIntentBuilder()
-                                    .setIsSmartLockEnabled(false)
-                                    .setAvailableProviders(Arrays.asList(
-                                            new AuthUI.IdpConfig.EmailBuilder().build(),
-                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
-                                    .build(),
-                            RC_SIGN_IN);
-                }
-            }
-        };
-        Button btn1 = (Button) findViewById(R.id.SignOut);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-            }
-        });
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
 
     }
 
-
     @Override
     protected void onPause(){
+        Log.d("main", "MainActivity: in onPause \n");
         super.onPause();
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
     @Override
     protected void onResume(){
+        Log.d("main", "MainActivity: in onResume \n");
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
-        //Check
     }
 }
